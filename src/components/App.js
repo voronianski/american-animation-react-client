6cd9a3c687676f2587b14dcfe53d7fe4b3a4e6eb
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-12-11 13:10:40 +0100

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <div className="app">Test run</div>
+  </Router>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+persistCache({
+  cache,
+  storage: window.localStorage,
+  key: 'american-animation-cache',
+  maxSize: false
+});
+
+const apolloClient = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'https://american-animation-graphql-api.herokuapp.com/graphql'
+  })
+});
+
+ReactDOM.render(
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
