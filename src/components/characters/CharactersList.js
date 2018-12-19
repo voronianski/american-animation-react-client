71ca1e7eb4b58ef30b0db9b0cea4b301b9a95acd
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-12-19 21:24:31 +0100

    add character list basics

--- src/components/characters/CharactersList.js
+++ src/components/characters/CharactersList.js
@@ -9,18 +9,19 @@ import './_characters-list.scss';
 function renderImageMosaic(videos) {
   const renderImage = (video, index) => {
     const indexClassMap = [
-      'absolute top-0 left-0',
-      'absolute top-0 right-0',
-      'absolute bottom-0 left-0',
-      'absolute bottom-0 right-0'
+      'absolute top-0 left-0 tl',
+      'absolute top-0 right-0 tr',
+      'absolute bottom-0 left-0 bl',
+      'absolute bottom-0 right-0 br'
     ];
     const imageClassNames = classNames(
-      'characters-list-item-mosaic-image rounded',
+      'characters-list-item-mosaic-image',
       !isNaN(index) ? indexClassMap[index] : 'single'
     );
 
     return (
       <div
+        key={video.id}
         className={imageClassNames}
         style={{ backgroundImage: `url(${video.image})` }}
       />
@@ -48,14 +49,16 @@ const CharactersList = ({ characters }) => {
         >
           <Link
             to={`/characters/${character.id}`}
-            className="characters-list-item-inner block p2 rounded"
+            className="characters-list-item-inner block p2"
           >
-            <div className="characters-list-item-overlay absolute top-0 left-0 right-0 bottom-0 z2 rounded" />
+            <div className="characters-list-item-overlay absolute top-0 left-0 right-0 bottom-0 z2" />
+
             {character.videos.length ? (
-              <div className="characters-list-item-mosaic absolute top-0 left-0 right-0 bottom-0 z1 rounded">
+              <div className="characters-list-item-mosaic absolute top-0 left-0 right-0 bottom-0 z1">
                 {renderImageMosaic(character.videos)}
               </div>
             ) : null}
+
             <div className="characters-list-item-details relative z3">
               <h3 className="characters-list-item-title h4 mt0">
                 <span className="characters-list-item-name">
@@ -65,12 +68,16 @@ const CharactersList = ({ characters }) => {
                   ({character.createdIn})
                 </span>
               </h3>
+
               {character.videos.length ? (
                 <div className="characters-list-item-videos caps">
                   <span role="img" aria-label="high voltage emoji">
                     ⚡️
                   </span>
-                  <span>{character.videos.length} cartoons</span>
+                  <span>
+                    {character.videos.length} cartoon
+                    {character.videos.length === 1 ? '' : 's'}
+                  </span>
                 </div>
               ) : null}
             </div>
--- src/pages/Cartoon.js
+++ src/pages/Cartoon.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import CartoonDetails from '../components/cartoons/CartoonDetails';
 import { addFavoritedCartoon, removeFavoritedCartoon } from '../store/actions';
 
-const Cartoon = props => {
-  const {
-    match,
-    isFavorited,
-    addFavoritedCartoon,
-    removeFavoritedCartoon
-  } = props;
+class Cartoon extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
 
-  window.scrollTo(0, 0);
+  render() {
+    const {
+      match,
+      isFavorited,
+      addFavoritedCartoon,
+      removeFavoritedCartoon
+    } = this.props;
 
-  return (
-    <div className="cartoon-page container-inner-narrow mx-auto">
-      <CartoonDetails
-        id={match.params.id}
-        isFavorited={isFavorited}
-        onToggleFavorite={videoId => {
-          if (isFavorited) {
-            removeFavoritedCartoon(videoId);
-          } else {
-            addFavoritedCartoon(videoId);
-          }
-        }}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="cartoon-page container-inner-narrow mx-auto">
+        <CartoonDetails
+          id={match.params.id}
+          isFavorited={isFavorited}
+          onToggleFavorite={videoId => {
+            if (isFavorited) {
+              removeFavoritedCartoon(videoId);
+            } else {
+              addFavoritedCartoon(videoId);
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ favoritedCartoons }, { match }) => ({
   isFavorited: favoritedCartoons.includes(match.params.id)
