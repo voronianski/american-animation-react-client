235b31d784ed0592e5e78aa91054ac2dbea11160
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-12-20 13:47:22 +0100

    add dashboard page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import Studio from './pages/Studio';
 import Characters from './pages/Characters';
 import Character from './pages/Character';
 import About from './pages/About';
+import Dashboard from './pages/Dashboard';
 
 const App = () => (
   <Router>
@@ -24,17 +25,18 @@ const App = () => (
       <Header />
       <div className="container py3 mb4">
         <Switch>
+          <Route exact path="/" component={Dashboard} />
           <Route exact path="/cartoons" component={Cartoons} />
-          <Route path="/cartoons/favorites" component={Favorites} />
-          <Route path="/cartoons/50-greatest" component={FiftyGreatest} />
-          <Route path="/cartoons/tex-avery-red" component={TexAveryRed} />
+          <Route exact path="/cartoons/favorites" component={Favorites} />
+          <Route exact path="/cartoons/50-greatest" component={FiftyGreatest} />
+          <Route exact path="/cartoons/tex-avery-red" component={TexAveryRed} />
           <Route path="/cartoons/:id" component={Cartoon} />
           <Route exact path="/studios" component={Studios} />
           <Route path="/studios/:id" component={Studio} />
           <Route exact path="/characters" component={Characters} />
           <Route path="/characters/:id" component={Character} />
           <Route path="/about" component={About} />
-          <Route render={() => <Redirect to="/cartoons" />} />
+          <Route render={() => <Redirect to="/" />} />
         </Switch>
       </div>
     </div>
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div className="dashboard-page">
+    <h2 className="dashboard-title h1 mt0 mb2">Dashboard</h2>
+    <div>Lists and lists...</div>
+  </div>
+);
+
+export default Dashboard;
